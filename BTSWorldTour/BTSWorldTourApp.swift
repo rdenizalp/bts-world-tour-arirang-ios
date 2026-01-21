0e35ae716412240329b11d0cdd584310a923a16c
Deniz Alp <rdenizalp@outlook.com>
2026-01-22 00:57:01 +0300

    Initial Commit

---
+++ BTSWorldTour/BTSWorldTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  BTSWorldTourApp.swift
+//  BTSWorldTour
+//
+//  Created by Deniz Alp on 22.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct BTSWorldTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
